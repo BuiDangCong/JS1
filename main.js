b4a08d1f5cca8d4d181d07c3d3caddabd1628d58
BuiDangCong <128606372+BuiDangCong@users.noreply.github.com>
2024-06-17 06:49:18 +0700

    1st commit

---
+++ main.js
@@ -0,0 +1,9 @@
+let arr = new Array(1, 2, 3, 4, 5, 6);
+// cách 1 dùng for i
+for (let i = 0; i < arr.length; i++) {
+  console.log(i);
+}
+// cách 2 dùng for of
+for (let a of arr) {
+  console.log(a);
+}
